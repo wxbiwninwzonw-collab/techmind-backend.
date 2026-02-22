2d92a21b6d3fe88408f88b1487bc51656d71177a
wxbiwninwzonw-collab <wxbiwninwzonw@gmail.com>
2026-02-22 20:05:12 +0200

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const cors = require('cors'); // لازم السطر ده يكون موجود
-const app = express();
-
-app.use(cors()); // وده أهم سطر عشان يسمح للتطبيق يكلم السيرفر
-app.use(express.json());
